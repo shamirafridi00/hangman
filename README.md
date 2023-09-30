2bd945dda9a18526e0bb2ea8d28fb345d8308855
Shamir Afridi <shamirafridi00@gmail.com>
2023-09-30 17:46:26 +0500

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,85 @@
-# hangman
+# Hangman Game
+
+## Introduction
+
+This Hangman game is a simple command-line implementation in Ruby. It allows players to guess a hidden word one letter at a time. The game provides features to save and load games, offering flexibility and entertainment.
+
+This project is part of The Odin Project curriculum, designed to help learners practice and improve their Ruby programming skills.
+
+## Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+  - [Starting a New Game](#starting-a-new-game)
+  - [Guessing Letters](#guessing-letters)
+  - [Saving a Game](#saving-a-game)
+  - [Loading a Game](#loading-a-game)
+- [Game Rules](#game-rules)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Prerequisites
+
+Before playing the Hangman game, ensure that you have Ruby installed on your computer. You can download and install Ruby from the official [Ruby website](https://www.ruby-lang.org/).
+
+## Installation
+
+1. Clone the Hangman game repository from GitHub:
+
+   ```bash
+   git clone https://github.com/your-username/hangman.git
+2. Navigate to the project directory:
+    ```bash
+    cd hangman
+3. Run the game by executing the main Ruby file:
+    ```bash
+    ruby main.rb
+
+## Usage
+
+### Starting a New Game
+- Upon running the game, it will display a welcome message and instructions.
+- You will start with a hidden word represented by underscores (e.g., _ _ _ _ _) and a limited number of guesses based on the word's length.
+- The available letters for guessing will be displayed.
+
+## Guessing Letters
+
+- To make a guess, enter a single lowercase letter.
+- If your guess is correct, it will replace the underscores with the correctly guessed letter(s).
+- If your guess is incorrect, your number of remaining guesses will decrease, and the incorrect letter will be added to the list of guessed letters.
+
+## Saving a Game
+
+- At any point during your turn, you can save the game by entering `save`.
+- The game will prompt you to enter a name for your save file.
+- The game state will be serialized and saved as a JSON file in a specified directory.
+
+## Loading a Game
+
+- To load a saved game, enter `load` when prompted to make a guess.
+- You will be asked to enter the name of the saved game file you want to load.
+- If a saved game with the provided name exists, it will load the game state, allowing you to continue from where you left off.
+
+## Game Rules
+
+- You must guess the entire word within a limited number of guesses to win.
+- The available guesses vary based on the length of the word.
+- If you run out of guesses without correctly guessing the word, you lose the game.
+
+## Contributing
+
+Contributions to the Hangman game project are welcome! If you'd like to contribute, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and test them thoroughly.
+4. Commit your changes with clear and concise messages.
+5. Push your changes to your fork.
+6. Submit a pull request to the main repository.
+
+## License
+
+This Hangman game project is licensed under the MIT License. You are free to use, modify, and distribute the code for both personal and commercial purposes.
+
+
--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,14 @@
+require 'json'
+require 'fileutils'  # To manage directories and files
+
+
+# Define the directory to store saved games
+SAVED_GAMES_DIR = 'saved_games'
+
+# Create the saved games directory if it doesn't exist
+FileUtils.mkdir_p(SAVED_GAMES_DIR)
+
+
 
 file_path = 'thousands-google-words.txt'
 words = File.read(file_path).split(' ') # return in array of strings
@@ -10,41 +21,58 @@ random_index = rand(filtered_words.length)
 
 # Load the selected word into your hash
 selected_word = filtered_words[random_index]
-p selected_word
+
 
 ################ METHODS ################
 
 def check_input(input)
 
-  if input.length == 1
+  if input.length == 1 || input == 'save' || input == 'load' || input == 'exit'
     puts "You entered: #{input}"
     input
   else
-    puts "Error: You can only enter one character, not more than that."
-    puts "Enter a letter: "
+    puts 'Error: You can only enter one character, not more than that. If you want to save the game, enter "save". If you want to load a game, enter "load". If you want to exit, enter "exit".'
+    puts 'Enter a letter: '
     check_input(gets.chomp.downcase)
   end
 end
 
+# Define a method to save the game state to a JSON file with a custom name
+def save_game(selected_word, display, lifes, guessed_letters, alphabet)
+  puts 'Enter a name for your save file: '
+  save_file_name = gets.chomp
+  save_file_path = File.join(SAVED_GAMES_DIR, "#{save_file_name}.json")
+
+  game_state = {
+    selected_word: selected_word,
+    display: display,
+    lifes: lifes,
+    guessed_letters: guessed_letters,
+    alphabet: alphabet
+  }
+
+  File.open(save_file_path, 'w') do |file|
+    file.write(JSON.generate(game_state))
+  end
+
+  puts "Game saved as #{save_file_name}."
+end
 
 
+# Define a method to load a saved game by specifying the file name
+def load_game_by_name(file_name)
+  save_file_path = File.join(SAVED_GAMES_DIR, "#{file_name}.json")
 
-
-
-
-
-
-
-
-
-
+  if File.exist?(save_file_path)
+    game_state = JSON.parse(File.read(save_file_path), symbolize_names: true)
+    game_state[:display] = game_state[:display].split('')  # Convert display back to an array
+    game_state
+  else
+    nil
+  end
+end
 
 ############################################
-
-
-
-
-
 
 
 dictionary = {}
@@ -55,32 +83,63 @@ puts "Random Word: #{selected_word}"
 puts "Hash: #{dictionary}"
 
 
+################  <<<<<<<<<   DISPLAY   >>>>>>>>>>>>      ############################
 display = Array.new(selected_word.length, '_')
+puts ('Weclome to Hangman!')
+puts ('Guess the word in given number of tries or less! or you will lose!')
+puts('Available guess are varies as per the lenght of the word which you need to be gussed')
 puts display.join('')
 
 lifes = selected_word.length
-puts "You are with #{lifes} guesses"
+puts "Your remaining guesses: #{lifes}"
 
 
 guessed_letters = []
-guessed_letters = []
-alphabet = ('a'..'z').to_a  # Create an array of all lowercase letters
+alphabet = ('a'..'z').to_a  #an array of all lowercase letters
+
+
+################  <<<<<<<<<   Main Loop   >>>>>>>>>>>>      ############################
 
 
 loop do
-  puts "Guessed letters: #{guessed_letters.join(' ')}"
+  puts "Guessed letters so far: #{guessed_letters.join(' ')}"
   puts "Available letters: #{alphabet.join(' ')}"
 
-  puts "Enter your guess: "
+  puts 'Enter your guess (or type "save" to save the game, "load" to load a saved game): '
   input = gets.chomp.downcase
   guess = check_input(input)
+
+  if input == 'save'
+    save_game(selected_word, display.join(''), lifes, guessed_letters, alphabet)
+  elsif input == 'load'
+    puts 'Enter the name of the saved game file you want to load: '
+    load_file_name = gets.chomp
+    loaded_game_state = load_game_by_name(load_file_name)
+
+    if loaded_game_state
+      selected_word = loaded_game_state[:selected_word]
+      display = loaded_game_state[:display]
+      lifes = loaded_game_state[:lifes]
+      guessed_letters = loaded_game_state[:guessed_letters]
+      alphabet = loaded_game_state[:alphabet]
+      puts 'Game loaded.'
+      puts display.join(' ')
+
+    else
+      puts 'No saved game found with that name.'
+      end
+
+
+  elsif input == 'exit'
+    break
+  end
+
+
 
   if alphabet.include?(guess) && !guessed_letters.include?(guess)
     guessed_letters << guess
     alphabet.delete(guess)
     correct_guess = false
-
-
 
     selected_word.each_char.with_index do |char, index|
       if char == guess
@@ -90,7 +149,13 @@ loop do
     end
 
 
-    puts "Correct guess!" if correct_guess
+    if correct_guess
+      puts 'Good guess!'
+    else
+      lifes -= 1
+      puts 'Wrong Guess!'
+      puts "Guesses left: #{lifes}"
+    end
     puts display.join(' ')
 
 
@@ -101,9 +166,8 @@ loop do
 
 
 
-
   else
-    puts "Invalid guess. Please enter a single letter that you haven't guessed before."
+    puts "You entered already this letter. Please enter a single letter that you haven't guessed before."
     lifes -= 1
     guessed_letters << guess
     alphabet.delete(guess)
