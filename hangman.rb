14ac6d2b2d337ad43ac6bef271268aa23c706ffa
Shamir Afridi <shamirafridi00@gmail.com>
2023-09-30 12:51:51 +0500

    Added: correct guess word fuctionality

--- hangman.rb
+++ hangman.rb
@@ -12,6 +12,41 @@ random_index = rand(filtered_words.length)
 selected_word = filtered_words[random_index]
 
 
+################ METHODS ################
+
+def check_input(input)
+
+  if input.length == 1
+    puts "You entered: #{input}"
+    input
+  else
+    puts "Error: You can only enter one character, not more than that."
+    puts "Enter a letter: "
+    check_input(gets.chomp.downcase)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################################
+
+
+
+
+
+
+
 dictionary = {}
 dictionary[:random_word] = selected_word
 
@@ -20,18 +55,56 @@ puts "Random Word: #{selected_word}"
 puts "Hash: #{dictionary}"
 
 
-display = Array.new(selected_word.length, '_').join(' ')
+display = '_' * selected_word.length
 p display
 
+lifes = selected_word.length
+puts "You are with #{lifes} guesses"
 
 
-puts 'Enter your guess'
-guess = gets.chomp
-
-if dictionary[:random_word].include?(guess)
-  puts 'yes it exists'
+guessed_letters = []
 
 
-else
-  puts 'no it does not exist'
+loop do
+  puts "Guessed letters: #{guessed_letters.join(' ')}"
+  puts display
+
+  puts "Enter your guess: "
+  input = gets.chomp.downcase
+  guess = check_input(input)
+
+  if dictionary[:random_word].include?(guess) && guess.match?(/[a-z]/) && !guessed_letters.include?(guess)
+    guessed_letters << guess
+    correct_guess = false
+
+
+
+    selected_word.each_char.with_index do |char, index|
+      if char == guess
+        p display[index] = guess
+        guessed_letters << guess
+        correct_guess = true
+        end
+    end
+
+
+    if display == selected_word
+      puts "Congratulations! You guessed the word: #{selected_word}"
+      break
+    end
+
+    puts "Correct guess!" if correct_guess
+
+
+
+  else
+    puts 'no it does not exist'
+    puts "Guesses left: #{lifes = lifes- 1}"
+  end
+
 end
+
+
+# display.gsub!('_', guess)
+# guessed_letters = []
+# p display
